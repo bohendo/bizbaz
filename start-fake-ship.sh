911b144b5ad9a92ed368cf39b0f1c9c4a9282c78
bohendo <bohendo@skiff.com>
2023-09-13 20:12:00 -0400

    generalize to create fake ships of different names

--- start-fake-ship.sh
+++ start-fake-ship.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
+name="${1:-zod}"
 root=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
 data="$root/data"
-name="zod"
-fresh="freshzod"
+fresh="fresh-$name"
 reset="${2:-"false"}" # or "true" to reset all persistent data to fresh state
 
 if [[ "$1" == "--reset" ]]
@@ -22,7 +22,7 @@ then
   if ! grep -q "$image_version" <<<"$(docker image ls | grep "$image_name")"
   then docker image pull "$image"
   fi
-  urbit="docker run --interactive --tty --rm --name=zod --mount=type=bind,src=$data,dst=/urbit --publish=8080:80 --entrypoint=urbit $image --loom 31"
+  urbit="docker run --interactive --tty --rm --name=$name --mount=type=bind,src=$data,dst=/urbit --publish=8080:80 --entrypoint=urbit $image --loom 31"
 else
   echo "Neither urbit nor docker is installed, can't start a fake ship" && exit 1
 fi
@@ -33,7 +33,7 @@ if [[ -d "$fresh" ]]
 then
   if [[ ! -d "$name" ]]
   then
-    echo "Copying fresh zod data from $fresh to $name"
+    echo "Copying fresh data from $fresh to $name"
     sudo chown -R "$(whoami)" .
     cp -r "$fresh" "$name"
   elif [[ "$reset" == "true" ]]
@@ -50,16 +50,15 @@ else
   echo "No data found at $(realpath $fresh)"
   echo "Booting a new urbit (+ dev-latest.pill) to use as a quick-start $name.."
   echo "Once this ship boots, run the following commands to pre-configure the fresh data dir"
-  echo "~zod:dojo> |mount %base" # generic boilerplate (like what you get after `git init`)
-  echo "~zod:dojo> |mount %garden" # this desk serves landscape so the server I guess?
-  echo "~zod:dojo> |mount %landscape" # landscape gui I think
-  echo "~zod:dojo> |mount %webterm" # in-browser dojo terminal app
-  echo "~zod:dojo> |exit"
-  echo "Then, re-run this script to start a fresh zod ship from the generated fresh"
+  echo "~$name:dojo> |mount %base" # generic boilerplate (like what you get after `git init`)
+  echo "~$name:dojo> |mount %garden" # this desk serves landscape so the server I guess?
+  echo "~$name:dojo> |mount %webterm" # in-browser dojo terminal app
+  echo "~$name:dojo> |exit"
+  echo "Then, re-run this script to start a fresh $name ship from the generated fresh"
   echo
   sleep 3 # give the user a sec to read the message above
   pill="dev-latest.pill"
-  if [[ ! -f "pill" ]]
+  if [[ ! -f "$pill" ]]
   then
     echo "Downloading $pill"
     wget https://storage.googleapis.com/media.urbit.org/developers/dev-latest.pill
