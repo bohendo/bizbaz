9db734b29d3002d1bc48e95160f7fa63e22820c6
shivhendo <shivhendo@proton.me>
2023-09-28 16:00:55 -0400

    init routes

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './app';
 import './index.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/apps/bizbaz/",
+    element: <App />,
+  },
+]);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
   document.getElementById('app')
 );
